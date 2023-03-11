7b7e43f1da166de03ffc1190064f914b6c86d797
sarra Mimouni <sarra.mimouni@hotmail.fr>
2023-03-11 23:09:56 +0100

    créer 1 html

---
+++ ex01/ft_print_alphabet.js
@@ -0,0 +1,6 @@
+/*
+Écrire une fonction qui affiche l’alphabet en majuscule, une string
+contenant l’alphabet en minuscule sera passée en paramètre la
+fonction. Chaque lettre devra être stockée dans une cellule du
+tableau.
+*/
