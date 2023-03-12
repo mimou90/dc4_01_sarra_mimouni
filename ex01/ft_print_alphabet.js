46f5ffa30cf24114a1eb09d2e4b0b334709480ec
sarra Mimouni <sarra.mimouni@hotmail.fr>
2023-03-12 15:35:07 +0100

    afficher l'alphabet en majuscule

--- ex01/ft_print_alphabet.js
+++ ex01/ft_print_alphabet.js
@@ -4,3 +4,16 @@ contenant l’alphabet en minuscule sera passée en paramètre la
 fonction. Chaque lettre devra être stockée dans une cellule du
 tableau.
 */
+
+function exo_un_alphabet (alphabet) {
+    var alphabet_miniscule = "abcdefghijklmnopqrstuvwxyz";
+    var alphabet_majuscule = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var tableau = [];
+    for (var a = 0; a < alphabet.length; a++) {
+        var lettre = alphabet [a];
+        var index = alphabet_miniscule.indexOf(lettre);
+        var lettre_majuscule = alphabet_majuscule[index];
+        tableau.push(lettre_majuscule);
+    }
+    console.log(tableau);
+}
