488f21f8f1b8e6d19b5295b4916cdf6cc2929a24
sarra Mimouni <sarra.mimouni@hotmail.fr>
2023-03-12 20:57:58 +0100

    tableau filtre

---
+++ ex04/ft_print_filter_array.js
@@ -0,0 +1,10 @@
+/* Écrire une fonction qui filtre tous les nombres impairs d’un tableau. 
+Affichez le résultat dans la console de développement 
+*/
+
+function exo_quatre_filtre (tableau) {
+    let tableauFiltres = tableau.filter(nombre => {
+        return (fruit != 'pair')
+    }) 
+console.log(tableauFiltres)
+}
