440871e16eeda0169fb9a7042347ea1546dfea1f
sarra Mimouni <sarra.mimouni@hotmail.fr>
2023-03-12 19:21:53 +0100

    tableau +1

---
+++ ex03/ft_print_update_array.js
@@ -0,0 +1,9 @@
+/*Écrire une fonction qui reçoit un tableau de chiffre en paramètre, qui 
+ajoute plus un à la valeur de chaque cellule et qui affiche le tableau 
+mis à jour*/
+
+function exo_trois_tableau (tableau) {
+    const tableau_deux =tableau.map( nombre => nombre +1);
+    console.log(tableau_deux)
+    
+}
