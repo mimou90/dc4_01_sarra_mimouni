e3c0b6720e8619d247c8dbe7ed63765fa2a611f3
sarra Mimouni <sarra.mimouni@hotmail.fr>
2023-03-12 21:42:24 +0100

    insert number

---
+++ ex05/ft_insert_number.js
@@ -0,0 +1,7 @@
+/* Écrire une fonction qui insert dans un tableau un nombre reçu en 
+paramètre dans l’ordre croissant. Le tableau de référence sera le 
+suivant :
+let tab = [0,1,2,3,4,5,6,7,8,9]
+*/
+
+let tab = [0,1,2,3,4,5,6,7,8,9] 
